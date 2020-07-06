4e39a2a3666c35e5fab8136a7a1d359355c0d2f4
Sambit Giri <sambitgiri@Sambits-MacBook-Pro.local>
2020-07-06 12:21:51 +0200

    py2 -> py3

--- src/usefuls.py
+++ src/usefuls.py
@@ -1,5 +1,5 @@
 import numpy as np
-import c2raytools as c2t
+import tools21cm as c2t
 
 
 def Mgrid_2_Msolar(M):
@@ -17,9 +17,9 @@ def put_sphere(array, centre, radius, label=1, periodic=True, verbose=False):
 	if periodic: 
 		RR2 = ((aw[:,0]+nx-centre[0])**2 + (aw[:,1]+ny-centre[1])**2 + (aw[:,2]+nz-centre[2])**2).reshape(array.shape)
 		array[RR2<=radius**2] = label
-		if verbose: print "Periodic circle of radius", radius, "made at", centre
+		if verbose: print("Periodic circle of radius", radius, "made at", centre)
 	else: 
-		if verbose: print "Non-periodic circle of radius", radius, "made at", centre
+		if verbose: print("Non-periodic circle of radius", radius, "made at", centre)
 	return array
 
 def put_circle(array, centre, radius, label=1, periodic=True, verbose=False):
@@ -31,7 +31,7 @@ def put_circle(array, centre, radius, label=1, periodic=True, verbose=False):
 	if periodic: 
 		RR2 = ((aw[:,0]+nx-centre[0])**2 + (aw[:,1]+ny-centre[1])**2).reshape(array.shape)
 		array[RR2<=radius**2] = label
-		if verbose: print "Periodic circle of radius", radius, "made at", centre
+		if verbose: print("Periodic circle of radius", radius, "made at", centre)
 	else: 
-		if verbose: print "Non-periodic circle of radius", radius, "made at", centre
+		if verbose: print("Non-periodic circle of radius", radius, "made at", centre)
 	return array
